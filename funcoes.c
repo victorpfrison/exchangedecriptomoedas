ed2faf6ade11972f34a5c11c7d36033e9dc35f16
estercode <est3r.pereira@gmail.com>
2024-09-24 00:31:40 +0000

    estrutura do progama e login/cadastro do usuario

---
+++ funcoes.c
@@ -0,0 +1,82 @@
+#include "funcoes.h"
+#include <stdio.h>
+#include <string.h>
+
+
+RESULTADOS login(Usuario usuarios[], int numUsuarios, char cpf[], char senha[]){
+    for (int i = 0; i < numUsuarios; i++) {
+        if (strcmp(usuarios[i].cpf, cpf) == 0 && strcmp(usuarios[i].senha, senha) == 0) {
+            printf("BEM-VINDO(A)!\n");
+            return i; // Retorna o índice do usuário logado
+        }
+    }
+    return -1; // CPF ou senha inválidos
+}
+
+RESULTADOS  carregarUsuario(Usuario usuarios[], int *numUsuarios){
+     FILE *file = fopen("usuarios.bin", "rb");
+    if (file) {
+        *numUsuarios = fread(usuarios, sizeof(Usuario), 100, file);
+        fclose(file);
+    }
+}
+RESULTADOS cadastrarUsuario(Usuario usuarios[], int *numUsuarios) {
+    Usuario novoUsuario;
+    printf("Digite seu CPF: ");
+    scanf("%s", novoUsuario.cpf);
+
+    printf("Digite sua senha: ");
+    scanf("%s", novoUsuario.senha);
+
+    //saldos do novo usuario
+    novoUsuario.saldoReais = 0.0;
+    novoUsuario.saldoBitcoin = 0.0;
+    novoUsuario.saldoEthereum = 0.0;
+    novoUsuario.saldoRipple = 0.0;
+
+    usuarios[(*numUsuarios)++] = novoUsuario;
+    printf("Cadastro realizado com sucesso! Agora faça o login\n");
+    salvarUsuario(usuarios, &numUsuarios);// Passando numUsuarios como ponteiro
+    return 0;
+}
+
+RESULTADOS salvarUsuario(Usuario usuarios[], int *numUsuarios) {
+    FILE *file = fopen("usuarios.bin", "wb");
+    if (file) {
+        fwrite(usuarios, sizeof(Usuario) ,*numUsuarios, file); // Usar *numUsuarios
+        fclose(file);
+        return OK;
+    } else {
+        printf("[ERRO] Não foi possível abrir o arquivo.\n");
+        return ARQUIVO_NAO_ENCONTRADO;
+    }
+}
+
+
+RESULTADOS consultarSaldo(Usuario *usuario){
+  printf("Consultar saldo\n");
+}
+RESULTADOS consultarExtrato(Usuario *usuario){
+  printf("Consultar extrato\n");
+}
+RESULTADOS depositarReais(Usuario *usuario, float valor){
+  printf("Depositar reais\n");
+}
+RESULTADOS sacarReais(Usuario *usuario, float valor, char senha []){
+  printf("Sacar reais\n");
+}
+RESULTADOS comprarCriptomoeda(Usuario *usuario, float valor, char senha [], char nomeCripto[]){
+  printf("Comprar criptomoeda\n");
+}
+RESULTADOS venderCriptomoeda(Usuario *usuario, float valor, char senha [], char nomeCripto[]){
+   printf("Vender criptomoeda\n");
+
+}
+RESULTADOS atualizarCotacao(){
+   printf("Atualizar cotacao\n");
+}
+void clearBuffer() {
+  int c;
+  while ((c = getchar()) != '\n' && c != EOF)
+    ;
+}
---
+++ main.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include "funcoes.h"
+
+typedef RESULTADOS (*funcoes)(Usuario*, void *);
+int main(){
+  // funcoes criada na funcoes.h para as operacoes
+  funcoes funcao[] = {
+      consultarSaldo, 
+      consultarExtrato, 
+      depositarReais, 
+      sacarReais, 
+      comprarCriptomoeda, 
+      venderCriptomoeda, 
+      atualizarCotacao
+  };
+  Usuario usuarios[MAXUSUARIO];
+  int numUsuarios = 0;
+  char cpf[12];
+  char senha[6];
+  int usuarioLogon = -1;
+  //carregando usuarios cadastrados
+  carregarUsuario(usuarios, &numUsuarios);
+  // efetuando o login
+  while (usuarioLogon == -1) {
+        printf("PROJETO 1 - EXCHANGE DE CRIPTOMOEDAS\n");
+        printf("{1} para login\n");
+        printf("{2} para cadastrar\n");
+        printf("{0} SAIR\n");
+        printf("DIGITE AQUI: ");
+        int opcao;
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                printf("Digite seu CPF: ");
+                scanf("%s", cpf);
+                clearBuffer();
+                printf("Digite sua senha: ");
+                scanf("%s", senha);
+                clearBuffer();
+                usuarioLogon = login(usuarios, numUsuarios, cpf, senha);
+                if (usuarioLogon == -1) {
+                    printf("CPF ou senha inválidos. Tente novamente.\n");
+                }
+              break;
+            case 2:
+              cadastrarUsuario(usuarios, &numUsuarios);
+              break;
+            case 0:
+              printf("Saindo do programa...\n");
+              return 0;
+          default:
+              printf("Opção inválida.\n");
+      }
+  }
+    int opcao;
+    do{
+      printf("\n\tMENU PRINCIPAL\n\t\n");
+      printf("\t1 - Consultar saldo\n\t");
+      printf("2 - Consultar extrato\n\t");
+      printf("3 - Depositar reais\n\t");
+      printf("4 - Sacar reais\n\t");
+      printf("5 - Comprar criptomoeda\n\t");
+      printf("6 - Vender criptomoeda\n\t");
+      printf("7 - Atualizar cotacao\n\t");
+      printf("0 - Sair\n\t");
+      printf("\nEscolha uma opção: ");
+      scanf("%d", &opcao);
+
+     switch(opcao){
+       case 1:
+        funcao[0]; // chamando a funcao consultarSaldo
+       break;
+       case 2:
+        funcao[1]; // chamando a funcao consultarExtrato
+       break;
+       case 3:
+        funcao[2]; // chamando a funcao depositarReais
+       break;
+       case 4:
+        funcao[3]; // chamando a funcao sacarReais
+       break;
+       case 5:
+        funcao[4]; // chamando a funcao comprarCriptomoeda
+       break;
+       case 6:
+        funcao[5]; // chamando a funcao venderCriptomoeda
+       break;
+       case 7:
+        funcao[6]; // chamando a funcao atualizarCotacao
+       break;
+       case 0:
+          salvarUsuario(usuarios, &numUsuarios);
+          printf("Saindo. ATÉ A PRÓXIMA\n");
+         return 0;
+       break;
+       default:
+       printf("OPÇÃO INVALIDA");
+     }
+    }
+
+while(opcao != 0);
+salvarUsuario(usuarios, &numUsuarios);
+return 0;
+
+}
